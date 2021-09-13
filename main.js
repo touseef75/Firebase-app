65be0db03a3e98b67cca89dc7fdaa782bf70993c
touseef75 <imrantouseef776@gmail.com>
2021-09-13 01:05:10 -0700

    creste s signup ui

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+let signup =document.getElementById("signup");
+signup.onclick = function(){
+    alert("S")
+}
